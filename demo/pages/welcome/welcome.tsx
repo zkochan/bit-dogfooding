417d440e866ec68414c42295b2ee1908b7174c65
Zoltan Kochan <z@kochan.io>
2021-10-21 19:53:48 +0300

    wip

--- demo/pages/welcome/welcome.tsx
+++ demo/pages/welcome/welcome.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Card } from '@company/zoltan.dogfooding.ui.card';
-import { Heading } from '@company/zoltan.dogfooding.ui.heading';
-import { Text } from '@company/zoltan.dogfooding.ui.text';
+import { Card } from '@zoltan/dogfooding.ui.card';
+import { Heading } from '@zoltan/dogfooding.ui.heading';
+import { Text } from '@zoltan/dogfooding.ui.text';
 import styles from './welcome.module.scss';
 
 export function Welcome() {
